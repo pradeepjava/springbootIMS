08ead7a70453ecc785893ea5bd9cd1e5b0033ed9
pradeepjava <pradeepkumar25aug@gmail.com>
2021-06-06 09:45:41 +0530

    base commit

---
+++ imsspringboot/src/main/java/com/ims/ImsspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsspringbootApplication.class, args);
+	}
+
+}
